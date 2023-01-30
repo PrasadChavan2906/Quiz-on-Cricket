f281251173343c47586bee22da9ed084661fcd16
PrasadChavan29 <114991527+PrasadChavan2906@users.noreply.github.com>
2023-01-30 06:40:29 +0000

    Added Qusetion

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const readline = require("readline");
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 console.log("Welcome to PRASAD's Cricket Quiz");
 
 const queBank = [
-	{
-		question: `
+  {
+    question: `
   1. The first official international match of cricket was held in 1844 between …
 	
    A: India and Afghanistan
@@ -17,10 +17,10 @@ const queBank = [
    B: England and Australia
 
    C: The United States and Canada\n`,
-		answer: "C"
-	},
-	{
-		question: `
+    answer: "C"
+  },
+  {
+    question: `
   2. In cricket, the Duckworth-Lewis method is …
    
    A: The awarding system for victory in matches interrupted by the weather
@@ -28,10 +28,10 @@ const queBank = [
    B: A training schedule in which the batsmen practice blindfolded
 
    C: A tactic that requires every fielder to be placed on a side\n`,
-		answer: "A"
-	},
-	{
-		question: `
+    answer: "A"
+  },
+  {
+    question: `
   3. … has the most record partnerships in the history of ICC Cricket World cup?
 
    A: Australia
@@ -39,10 +39,10 @@ const queBank = [
    B: India
 
    C: England\n`,
-		answer: "B"
-	},
-	{
-		question: `
+    answer: "B"
+  },
+  {
+    question: `
   4. … is not an official way of being dismissed in a cricket match
 	 
    A: Hit Wicket
@@ -52,53 +52,66 @@ const queBank = [
     C: Six & Out
 
 \n`,
-		answer: "C"
-	}
+    answer: "C"
+  }
+  {
+    question: `
+  5. … is the bowler who has the greatest innings figures during a test match
+	
+   A. Anil Kumble
+
+   B. Jim Laker
+
+   C. Muttiah Muralitharan
+
+\n`,
+    answer: "B"
+  }
 ];
 
 let questionIndex = 0;
 
 function question() {
-	rl.question(queBank[questionIndex].question, (answer) => {
-		console.log(`You answered: ${answer}`);
+  rl.question(queBank[questionIndex].question, (answer) => {
+    console.log(`You answered: ${answer}`);
 
-		if (answer.toLowerCase() == queBank[questionIndex].answer.toLowerCase()) {
-			console.log("right answer");
-			questionIndex++;
-			serve();
-		} else {
-			console.log("wrong answer");
-			console.log("try again");
-			question();
-		}
-	});
+    if (answer.toLowerCase() == queBank[questionIndex].answer.toLowerCase()) {
+      console.log("right answer");
+      questionIndex++;
+      serve();
+    } else {
+      console.log("wrong answer");
+      console.log("try again");
+      question();
+    }
+  });
 }
 
 function serve() {
-	if (isQuestionBankExhausted()) {
-		console.log("thanks for playing....");
-		rl.close();
-	} else {
-		rl.question("what do you want to do? \n Press e to exit, any other key to continue....  ", (choice) => {
-			console.log(`You selected ${choice}`);
+  if (isQuestionBankExhausted()) {
+    console.log("thanks for playing....");
+    rl.close();
+  } else {
+    rl.question("what do you want to do? \n Press e to exit, any other key to continue....  ", (choice) => {
+      console.log(`You selected ${choice}`);
 
-			if (choice.toLowerCase() !== "e") {
-				question();
-			} else {
-				console.log("thanks for playing....");
-				rl.close();
-			}
-		});
-	}
+      if (choice.toLowerCase() !== "e") {
+        question();
+      } else {
+        console.log("thanks for playing....");
+        rl.close();
+      }
+    });
+  }
 }
 
 /**
  * checks array length with index and exits game
  */
 function isQuestionBankExhausted() {
-	if (queBank.length == questionIndex) {
-		return true;
-	}
+  if (queBank.length == questionIndex) {
+    return true;
+  }
 }
 
 serve();
